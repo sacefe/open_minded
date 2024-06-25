0cc06ad164b9abdb30be1409575ea9a7ef1d18d0
Alex <alex.tseng@innovusion.com>
2024-06-25 10:58:21 -0700

    new array problem

---
+++ src/arrays/multiply_Strings_43.py
@@ -0,0 +1,45 @@
+'''
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ 
+
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+ 
+
+Constraints:
+
+1 <= num1.length, num2.length <= 200
+num1 and num2 consist of digits only.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+'''
+
+class Multiply():
+    def multiply(self, num1: str, num2: str) -> str:
+        inum1=0
+        inum2=0
+        base= 1
+        for i in range(len(num1)-1, -1, -1 ):
+            inum1 += int(num1[i]) * base
+            base *= 10 
+
+        base= 1
+        for i in range(len(num2)-1, -1, -1 ):
+            inum2 += int(num2[i]) * base
+            base *= 10 
+
+        return  str(inum2*inum1)
+    
+if __name__ == '__main__':
+    num1 = "123" 
+    num2 = "456"
+    obj= Multiply()
+    print(obj.multiply(num1, num2)) 
